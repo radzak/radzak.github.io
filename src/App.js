d67d4505aa735b0645c0163f8f8b09bff71c8c55
Radek Krzak <radek.krzak@gmail.com>
2018-03-29 17:35:41 +0200

    Add loading animation (with fixed timeout temporarily)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,33 @@ import About from '@/views/About';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    // setTimeout(() => this.setState({ loading: false }), 1000);
+
+    // TODO: don't use fixed timeout, instead check if the #root is not empty
+    const ele = document.getElementById('ipl-progress-indicator');
+    if (ele) {
+      setTimeout(() => {
+        ele.classList.add('available');
+        this.setState({ loading: false });
+        setTimeout(() => {
+          ele.outerHTML = '';
+        }, 2000);
+      }, 600);
+    }
+  }
+
   render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      return null; // render null when app is not ready
+    }
+
     return (
       <Layout>
         <Switch>
