b02b5660ca175e4488717f8c8eac852885f3e708
radzak <radek.krzak@gmail.com>
2018-04-10 15:59:56 +0200

    Add CV (in .png, no zoom yet)

--- src/views/CV/index.js
+++ src/views/CV/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 
+import CvPDF from "@/assets/Radek's Resume.png";
 import Page from '@/views/components/Page';
+
 import styles from './styles.css';
 
 const cx = classNames.bind(styles);
 
 const CV = props => (
   <Page>
-    <div className={cx('banner')}>
-      <h1>CV</h1>
+    <div className={cx('cv')}>
+      <img src={CvPDF} alt="Radek's Resume" />
     </div>
   </Page>
 );
