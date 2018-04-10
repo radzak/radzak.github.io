0386dc528feffe5c621280906940f8f271c2c645
Radek Krzak <radek.krzak@gmail.com>
2018-04-10 14:29:22 +0200

    Change styles on Contact view, change html structure on Home view

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,51 +1,23 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 
 import Page from '@/views/components/Page';
-import Typewriter from '@/components/Typewriter';
 import BouncyText from '@/components/BouncyText';
 import styles from './styles.css';
 
 const cx = classNames.bind(styles);
 
+// eslint-disable-next-line react/prefer-stateless-function
 class Home extends Component {
-  state = {
-    typing: true
-  };
-
-  handleTypingFinish = () => {
-    this.setState({ typing: false });
-  };
-
-  words = [
-    'Hey!',
-    'Looking for someone',
-    'smart?',
-    'curious?',
-    'ingenious?',
-    'cunning?',
-    'Good job, You found me!'
-  ];
-
   render() {
     return (
       <Page>
         <div className={cx('banner')}>
-          {this.state.typing ? (
-            <h1>
-              <Typewriter words={this.words} period={800} onFinish={this.handleTypingFinish} />
-            </h1>
-          ) : (
-            <Fragment>
-              <h1>
-                <BouncyText text="Hi!" />
-                <br />
-                My name is Radek Krzak
-              </h1>
-              <br />
-              <h2>and I&apos;m a Full Stack Developer</h2>
-            </Fragment>
-          )}
+          <h1>
+            <BouncyText text="Hi!" />
+          </h1>
+          <h1>My name is Radek Krzak</h1>
+          <h2>and I&apos;m a Full Stack Developer</h2>
         </div>
       </Page>
     );
